f7a4b8662e76039b9faaf6ed1d53508da95b849b
Allan Vigiani <vigianiallan@gmail.com>
2020-06-09 11:16:31 -0300

    error

---
+++ Calculator/script.js
@@ -0,0 +1,12 @@
+function calculate(type, value){
+    //type - Action or Value
+    //value - Number or Operator
+    if (type === 'action') {
+
+    } else if (type === 'value'){
+
+        document.querySelector('#result').value += value;
+
+    }
+
+}
