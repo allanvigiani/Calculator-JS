2fc5984e5862be97fe5543b108f693980f6dc72b
Allan Vigiani <vigianiallan@gmail.com>
2020-06-09 11:37:13 -0300

    final

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,9 +3,29 @@ function calculate(type, value){
     //value - Number or Operator
     if (type === 'action') {
 
+        if (value === 'c') {
+            //Clear display
+            document.getElementById('result').value = '';
+
+        } 
+
+        if (value === '+' || value === '-' || value === '*' || value === '/' || value === '.') {
+
+            document.getElementById('result').value += value;
+
+        }
+
+        if (value === '='){
+
+            var value_expression = eval(document.getElementById('result').value);
+            document.getElementById('result').value = value_expression;
+
+        }
+
+
     } else if (type === 'value'){
 
-        document.querySelector('#result').value += value;
+        document.getElementById('result').value += value;
 
     }
 
